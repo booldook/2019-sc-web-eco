4bb419cde537741e9db7f6ba48cb4eec950e4b6f
booldook <booldook@gmail.com>
2019-07-22 10:21:19 +0900

    tip

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ new WOW().init();
 
 // tooltip 시동
 $(".tooltip-bt").tooltip();
+
+$(".fa-user").mouseover(function(){
+	$(this).next().css({
+		"opacity":1, "transform":"translateY(0)"
+	});
+});
+$(".fa-user").mouseleave(function(){
+	$(this).next().css({
+		"opacity":0, "transform":"translateY(20px)"
+	});
+});
