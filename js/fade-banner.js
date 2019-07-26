8533b961138bf6539a07000562721ce4eec1e62e
booldook <booldook@gmail.com>
2019-07-26 15:28:52 +0900

    12.eco fade-banner 작업중

--- js/fade-banner.js
+++ js/fade-banner.js
@@ -1,7 +1,9 @@
 var adsNow = 0;
-$(".pager span").click(adsAni);
-function adsAni(){
+$(".pager span").click(function(){
 	adsNow = $(this).index();
+	adsAni();
+});
+function adsAni(){
 	$(".ads-img").stop().animate({"opacity": 0}, 2000);
 	$(".ads-img").eq(adsNow).stop().animate({"opacity": 1}, 2000);
 }
